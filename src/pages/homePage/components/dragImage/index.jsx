6423dad8bc5383104e70c7e9fcd5ec33baa4d528
serena_xu1 <serena_xu1@epam.com>
2021-08-26 15:45:22 +0800

    add success pic

--- src/pages/homePage/components/dragImage/index.jsx
+++ src/pages/homePage/components/dragImage/index.jsx
@@ -40,33 +40,67 @@ export default function DragImage() {
       // rect.set({ left: 20, top: 50 });
       // canvas.renderAll();
       fabric.Image.fromURL('/images/ladybug.png', function (img) {
+        // img.set({
+        //   left: fabric.util.getRandomInt(0, 600),
+        //   top: fabric.util.getRandomInt(0, 500),
+        //   angle: fabric.util.getRandomInt(0, 90)
+        // });
         img.set({
-          left: fabric.util.getRandomInt(0, 600),
-          top: fabric.util.getRandomInt(0, 500),
-          angle: fabric.util.getRandomInt(0, 90)
+          left: 200,
+          top: 600,
+          angle: 90
         });
-
         img.perPixelTargetFind = true;
         img.hasControls = img.hasBorders = false;
 
-        img.scale(fabric.util.getRandomInt(50, 100) / 100);
+        // img.scale(fabric.util.getRandomInt(50, 100) / 100);
 
         canvas.add(img);
       });
 
       // canvas.setBackgroundImage('/images/cut1.jpg', canvas.renderAll.bind(canvas));
+      canvas.on('object:moving', (options) => {
+        console.log(options.e.clientX, options.e.clientY);
+        if (options.e.clientX >= 380 && options.e.clientX <= 450 && options.e.clientY >= 150 && options.e.clientY <= 250) {
+          // var successDia = document.getElementsByClassName('successDialog');
+          // console.log('successDia: ', successDia);
+          // successDia.style.display = "block";
+
+          fabric.Image.fromURL('/images/success.jpg', function (img) {
+            // img.set({
+            //   left: fabric.util.getRandomInt(0, 600),
+            //   top: fabric.util.getRandomInt(0, 500),
+            //   angle: fabric.util.getRandomInt(0, 90)
+            // });
+            img.set({
+              left: 300,
+              top: 300,
+              angle: 0
+            });
+            img.perPixelTargetFind = true;
+            img.hasControls = img.hasBorders = false;
+
+            // img.scale(fabric.util.getRandomInt(50, 100) / 100);
+
+            canvas.add(img);
+          });
+          options.e.preventDefault();
+        }
+      });
     },
     [],
   )
 
   useEffect(() => {
-    // createCanvas();
     init();
   }, []);
 
   return (
     <div className="light-bg">
       <canvas id="canvas" width="300" height="300"></canvas>
+      <div className="successDialog">
+        回答正确！
+      </div>
     </div>
   );
 
